778160a1552e04e85bb33646775745e358b59891
hniksic <devnull@localhost>
2000-10-23 08:43:04 -0700

    [svn] hniksic@iskon.hr  ->  hniksic@arsdigita.com

--- src/main.c
+++ src/main.c
@@ -433,7 +433,7 @@ This program is distributed in the hope that it will be useful,\n\
 but WITHOUT ANY WARRANTY; without even the implied warranty of\n\
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n\
 GNU General Public License for more details.\n"));
-	  printf (_("\nOriginally written by Hrvoje Niksic <hniksic@iskon.hr>.\n"));
+	  printf (_("\nOriginally written by Hrvoje Niksic <hniksic@arsdigita.com>.\n"));
 	  exit (0);
 	  break;
 	case 'v':
