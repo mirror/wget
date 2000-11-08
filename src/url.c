f306ae96268c50d3899b2e5614fdf738eaea224a
hniksic <devnull@localhost>
2000-11-08 07:51:28 -0800

    [svn] Changed last_slash[-1] to *(last_slash - 1).

--- src/url.c
+++ src/url.c
@@ -1369,7 +1369,7 @@ construct (const char *url, const char *sub, int subsize, int no_proto)
 	      start_insert = end + 1;
 	      need_explicit_slash = 1;
 	    }
-	  else if (last_slash && last_slash != url && last_slash[-1] == '/')
+	  else if (last_slash && last_slash != url && *(last_slash - 1) == '/')
 	    {
 	      /* example: http://host"  */
 	      /*                      ^ */
