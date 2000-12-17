f6a41b566961d30d546b780e54cc5494349e8e48
hniksic <devnull@localhost>
2000-12-17 10:18:14 -0800

    [svn] Applied Csaba Raduly's patch from <802569B4.0043F7B1.00@Jagannath.uk.sophos>.

--- src/sysdep.h
+++ src/sysdep.h
@@ -45,15 +45,15 @@ Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  */
 #include <mswindows.h>
 #endif /* WINDOWS */
 
-/* Allegedly needed for compilation under OS/2: */
-#ifdef EMXOS2
+/* Needed for compilation under OS/2: */
+#ifdef __EMX__
 #ifndef S_ISLNK
 # define S_ISLNK(m) 0
 #endif
 #ifndef lstat
 # define lstat stat
 #endif
-#endif /* EMXOS2 */
+#endif /* __EMX__ */
 
 /* Reportedly, stat() macros are broken on some old systems.  Those
    systems will have to fend for themselves, as I will not introduce
