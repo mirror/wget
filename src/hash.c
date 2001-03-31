22240955e61e69117721daf0beabb58cbb974628
hniksic <devnull@localhost>
2001-03-30 21:07:10 -0800

    [svn] Undef xmalloc and friends before redefining them when standalone.

--- src/hash.c
+++ src/hash.c
@@ -30,6 +30,10 @@ Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  */
 #include "hash.h"
 
 #ifdef STANDALONE
+# undef xmalloc
+# undef xrealloc
+# undef xfree
+
 # define xmalloc malloc
 # define xrealloc realloc
 # define xfree free
