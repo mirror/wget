72b9bb6d922bef56ffa5f5627f38b274cd03bbb3
hniksic <devnull@localhost>
2001-04-30 03:19:41 -0700

    [svn] Zero out SIN before using it. Published in <sxs3daq8zdg.fsf@florida.arsdigita.de>.

--- src/init.c
+++ src/init.c
@@ -521,13 +521,14 @@ cmd_address (const char *com, const char *val, void *closure)
   struct sockaddr_in sin;
   struct sockaddr_in **target = (struct sockaddr_in **)closure;
 
+  memset (&sin, '\0', sizeof (sin));
+
   if (!store_hostaddress ((unsigned char *)&sin.sin_addr, val))
     {
       fprintf (stderr, _("%s: %s: Cannot convert `%s' to an IP address.\n"),
 	       exec_name, com, val);
       return 0;
     }
-
   sin.sin_family = AF_INET;
   sin.sin_port = 0;
 
