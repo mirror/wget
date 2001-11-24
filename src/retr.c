177b6ee8fd7967f5b8dfb39e5098e30dc185ca25
hniksic <devnull@localhost>
2001-11-24 06:02:10 -0800

    [svn] Print the actual error message when parsing of redirection URL fails.

--- src/retr.c
+++ src/retr.c
@@ -326,10 +326,11 @@ retrieve_url (const char *origurl, char **file, char **newloc,
       mynewloc = construced_newloc;
 
       /* Now, see if this new location makes sense. */
-      newloc_struct = url_parse (mynewloc, NULL);
+      newloc_struct = url_parse (mynewloc, &up_error_code);
       if (!newloc_struct)
 	{
-	  logprintf (LOG_NOTQUIET, "%s: %s.\n", mynewloc, "UNKNOWN");
+	  logprintf (LOG_NOTQUIET, "%s: %s.\n", mynewloc,
+		     url_error (up_error_code));
 	  url_free (newloc_struct);
 	  url_free (u);
 	  if (redirections)
