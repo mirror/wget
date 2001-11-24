212eb1ddafc345c3cb127cdd984ccfa73c9e7932
hniksic <devnull@localhost>
2001-11-23 18:38:47 -0800

    [svn] Fake a miniscule amount of download time when none is detected.

--- src/progress.c
+++ src/progress.c
@@ -439,8 +439,15 @@ static void
 bar_finish (void *progress)
 {
   struct bar_progress *bp = progress;
+  long elapsed = wtimer_elapsed (bp->timer);
 
-  create_image (bp, wtimer_elapsed (bp->timer));
+  if (elapsed == 0)
+    /* If the download was faster than the granularity of the timer,
+       fake some output so that we don't get the ugly "----.--" rate
+       at the download finish.  */
+    elapsed = 1;
+
+  create_image (bp, elapsed);
   display_image (bp->buffer);
 
   logputs (LOG_VERBOSE, "\n\n");
