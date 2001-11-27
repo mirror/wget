cca7541b1065b0aac5b0564753bd5d31a14698ca
hniksic <devnull@localhost>
2001-11-27 04:58:09 -0800

    [svn] Don't translate %d-%d.

--- src/url.c
+++ src/url.c
@@ -1833,8 +1833,7 @@ convert_links (const char *file, struct urlpos *links)
   fclose (fp);
   read_file_free (fm);
 
-  logprintf (LOG_VERBOSE,
-	     _("%d-%d\n"), to_file_count, to_url_count);
+  logprintf (LOG_VERBOSE, "%d-%d\n", to_file_count, to_url_count);
 }
 
 /* Construct and return a malloced copy of the relative link from two
