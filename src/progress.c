69c78db55b1beab21cc0009b44518a178e5aa986
hniksic <devnull@localhost>
2001-11-27 02:17:09 -0800

    [svn] Commence bar progress with two newlines.

--- src/progress.c
+++ src/progress.c
@@ -404,7 +404,7 @@ bar_create (long initial, long total)
   bp->width = screen_width;
   bp->buffer = xmalloc (bp->width + 1);
 
-  logputs (LOG_VERBOSE, "\n");
+  logputs (LOG_VERBOSE, "\n\n");
 
   create_image (bp, 0);
   display_image (bp->buffer);
