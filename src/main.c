1c906e47d3af155bafd7d439e546d1406473f8d7
hniksic <devnull@localhost>
2001-11-27 04:54:31 -0800

    [svn] Remove stray HAVE_RANDOM check from main.c.

--- src/main.c
+++ src/main.c
@@ -166,10 +166,6 @@ Download:\n\
   -Q,  --quota=NUMBER           set retrieval quota to NUMBER.\n\
        --limit-rate=RATE        limit download rate to RATE.\n\
 \n"), stdout);
-#ifdef HAVE_RANDOM
-  fputs (_("\
-\n"), stdout);
-#endif
   fputs (_("\
 Directories:\n\
   -nd  --no-directories            don\'t create directories.\n\
