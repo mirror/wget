7674ecd97eba5dbc08114f511058d231d00fdf4e
hniksic <devnull@localhost>
2001-11-30 00:02:33 -0800

    [svn] Simplify display_image.

--- src/progress.c
+++ src/progress.c
@@ -644,10 +644,7 @@ create_image (struct bar_progress *bp, long dltime)
 static void
 display_image (char *buf)
 {
-  char *del_buf = alloca (screen_width + 1);
-  memset (del_buf, '\b', screen_width);
-  del_buf[screen_width] = '\0';
-  logputs (LOG_VERBOSE, del_buf);
+  logputs (LOG_VERBOSE, "\r");
   logputs (LOG_VERBOSE, buf);
 }
 
