f5d20aef3213316efe3a9e3489bd00621fbe7826
hniksic <devnull@localhost>
2001-12-05 17:15:05 -0800

    [svn] Make the message printed in ssl_init_prng translatable.

--- src/gen_sslfunc.c
+++ src/gen_sslfunc.c
@@ -105,7 +105,8 @@ ssl_init_prng (void)
 
   if (RAND_status () == 0)
     {
-      logprintf (LOG_NOTQUIET, "Could not seed OpenSSL PRNG; disabling SSL.\n");
+      logprintf (LOG_NOTQUIET,
+		 _("Could not seed OpenSSL PRNG; disabling SSL.\n"));
       scheme_disable (SCHEME_HTTPS);
     }
 #endif /* SSLEAY_VERSION_NUMBER >= 0x00905100 */
