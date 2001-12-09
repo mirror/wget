e521ee38ffaacefe15bb51e8e8ceb0ee5eb576cc
hniksic <devnull@localhost>
2001-12-09 11:44:20 -0800

    [svn] Remove stray debugging message. Published in <sxszo4sxjpw.fsf@florida.arsdigita.de>.

--- src/main.c
+++ src/main.c
@@ -738,10 +738,7 @@ Can't timestamp and not clobber old files at the same time.\n"));
     {
       char *rewritten = rewrite_shorthand_url (argv[optind]);
       if (rewritten)
-	{
-	  printf ("Converted %s to %s\n", argv[optind], rewritten);
-	  url[i] = rewritten;
-	}
+	url[i] = rewritten;
       else
 	url[i] = xstrdup (argv[optind]);
     }
