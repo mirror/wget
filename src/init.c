0f120eee66b341439bda9d0e2108ec0d199778ce
hniksic <devnull@localhost>
2001-12-13 11:19:03 -0800

    [svn] Fix error message when $WGETRC not found. Published in <sxswuzrylnm.fsf@florida.arsdigita.de>.

--- src/init.c
+++ src/init.c
@@ -301,7 +301,7 @@ wgetrc_file_name (void)
     {
       if (!file_exists_p (env))
 	{
-	  fprintf (stderr, "%s: %s: %s.\n", exec_name, file, strerror (errno));
+	  fprintf (stderr, "%s: %s: %s.\n", exec_name, env, strerror (errno));
 	  exit (1);
 	}
       return xstrdup (env);
