566289ea9d890f6eabf4ba74dbbbb392835cdd67
hniksic <devnull@localhost>
2003-09-22 06:55:22 -0700

    [svn] Added comment about SO_SNDBUF for post.

--- src/connect.c
+++ src/connect.c
@@ -176,7 +176,6 @@ connect_to_one (ip_address *addr, unsigned short port, int silent)
   if (sock < 0)
     goto out;
 
-#ifdef SO_RCVBUF
   /* For very small rate limits, set the buffer size (and hence,
      hopefully, the size of the kernel window) to the size of the
      limit.  That way we don't sleep for more than 1s between network
@@ -186,10 +185,13 @@ connect_to_one (ip_address *addr, unsigned short port, int silent)
       int bufsize = opt.limit_rate;
       if (bufsize < 512)
 	bufsize = 512;
+#ifdef SO_RCVBUF
       setsockopt (sock, SOL_SOCKET, SO_RCVBUF,
 		  (char *)&bufsize, sizeof (bufsize));
-    }
 #endif
+      /* When we add opt.limit_rate support for writing, as with
+	 `--post-file', also set SO_SNDBUF here.  */
+    }
 
   resolve_bind_address ();
   if (bind_address_resolved)
