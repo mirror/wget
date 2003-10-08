6057dbec6f016d203c2b2fbdd5c7c2e96f7ef78a
hniksic <devnull@localhost>
2003-10-08 07:55:44 -0700

    [svn] Add definitions of getopt_long and getopt_long_only to getopt.c. The definitions were lost when updating the file.

--- src/getopt.c
+++ src/getopt.c
@@ -974,6 +974,33 @@ getopt (argc, argv, optstring)
 			   0);
 }
 
+int
+getopt_long (argc, argv, options, long_options, opt_index)
+     int argc;
+     char *const *argv;
+     const char *options;
+     const struct option *long_options;
+     int *opt_index;
+{
+  return _getopt_internal (argc, argv, options, long_options, opt_index, 0);
+}
+
+/* Like getopt_long, but '-' as well as '--' can indicate a long option.
+   If an option that starts with '-' (not '--') doesn't match a long option,
+   but does match a short option, it is parsed as a short option
+   instead.  */
+
+int
+getopt_long_only (argc, argv, options, long_options, opt_index)
+     int argc;
+     char *const *argv;
+     const char *options;
+     const struct option *long_options;
+     int *opt_index;
+{
+  return _getopt_internal (argc, argv, options, long_options, opt_index, 1);
+}
+
 #endif	/* Not ELIDE_CODE.  */
 
 #ifdef TEST
