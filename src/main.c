33dceca7841cab14152d52f30c329f6cd91e89a4
hniksic <devnull@localhost>
2003-10-08 08:24:56 -0700

    [svn] Fix typo in help string.

--- src/main.c
+++ src/main.c
@@ -219,7 +219,7 @@ HTTPS (SSL) options:\n\
        --sslcertfile=FILE     optional client certificate.\n\
        --sslcertkey=KEYFILE   optional keyfile for this certificate.\n\
        --egd-file=FILE        file name of the EGD socket.\n\
-       --sslcadir=DIR         dir where hash list of CA's are stured.\n\
+       --sslcadir=DIR         dir where hash list of CA's are stored.\n\
        --sslcafile=FILE       file with bundle of CA's\n\
        --sslcerttype=0/1      Client-Cert type 0=PEM (default) / 1=ASN1 (DER)\n\
        --sslcheckcert=0/1     Check the server cert agenst given CA\n\
