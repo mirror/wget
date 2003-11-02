c12e07faa611b88fc05734c54a7efa5c8e4b2c09
hniksic <devnull@localhost>
2003-11-02 13:16:43 -0800

    [svn] Cosmetic change.

--- src/connect.c
+++ src/connect.c
@@ -242,7 +242,7 @@ connect_to_ip (const ip_address *ip, int port, const char *print)
       const char *txt_addr = pretty_print_address (ip);
       if (print && 0 != strcmp (print, txt_addr))
 	logprintf (LOG_VERBOSE,
-		   _("Connecting to %s{%s}:%d... "), print, txt_addr, port);
+		   _("Connecting to %s|%s|:%d... "), print, txt_addr, port);
       else
 	logprintf (LOG_VERBOSE, _("Connecting to %s:%d... "), txt_addr, port);
     }
