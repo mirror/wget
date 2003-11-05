5355da917f8d210e18752b0c9c0231216513750f
hniksic <devnull@localhost>
2003-11-05 06:42:42 -0800

    [svn] More specific error message in resolve_bind_address.

--- src/connect.c
+++ src/connect.c
@@ -175,9 +175,10 @@ resolve_bind_address (const char *host, struct sockaddr *sa, int flags)
   al = lookup_host (host, flags | LH_SILENT | LH_PASSIVE);
   if (al == NULL)
     {
+      /* #### We should print the error message here. */
       logprintf (LOG_NOTQUIET,
-		 _("Unable to convert `%s' to a bind address.  Reverting to ANY.\n"),
-		 opt.bind_address);
+		 _("%s: unable to resolve bind address `%s'; disabling bind.\n"),
+		 exec_name, opt.bind_address);
       return 0;
     }
 
