bbcdd714840d3de40fcbb6736080b71ece17d663
hniksic <devnull@localhost>
2003-11-19 05:25:20 -0800

    [svn] Don't reference opt.ipv4_only and opt.ipv6_only if IPv6 is disabled.

--- src/main.c
+++ src/main.c
@@ -807,12 +807,14 @@ Can't timestamp and not clobber old files at the same time.\n"));
       print_usage ();
       exit (1);
     }
+#ifdef ENABLE_IPV6
   if (opt.ipv4_only && opt.ipv6_only)
     {
       printf (_("Cannot specify both --inet4-only and --inet6-only.\n"));
       print_usage ();
       exit (1);
     }
+#endif
 
   nurl = argc - optind;
   if (!nurl && !opt.input_filename)
