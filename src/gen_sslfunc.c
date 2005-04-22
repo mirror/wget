e09eb765f4387097cc734392e9bc765bec296fe8
hniksic <devnull@localhost>
2005-04-22 08:12:59 -0700

    [svn] Fix warning message text; mark the warning message as translatable.

--- src/gen_sslfunc.c
+++ src/gen_sslfunc.c
@@ -219,12 +219,14 @@ ssl_init ()
     {
       if (!can_validate)
 	{
-	  logprintf (LOG_NOTQUIET, "Warrining validation of Server Cert not possible!\n");
+	  logputs (LOG_NOTQUIET,
+		   _("Warning: validation of server certificate not possible!\n"));
 	  verify = SSL_VERIFY_NONE;
 	}
      else
 	{
-	  /* break handshake if server cert is not valid but allow NO-Cert mode */
+	  /* break handshake if server cert is not valid but allow
+	     NO-Cert mode */
 	  verify = SSL_VERIFY_PEER;
 	}
     }
