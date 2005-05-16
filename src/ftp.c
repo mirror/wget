6664409baacff2381b942137ab4fd631012c65ec
hniksic <devnull@localhost>
2005-05-16 05:41:29 -0700

    [svn] Consistently print \n at the end of the "Length" line.

--- src/ftp.c
+++ src/ftp.c
@@ -236,8 +236,7 @@ print_length (wgint size, wgint start, int authoritative)
 	logprintf (LOG_VERBOSE, _(", %s remaining"),
 		   with_thousand_seps (size - start));
     }
-  if (!authoritative)
-    logputs (LOG_VERBOSE, _(" (unauthoritative)\n"));
+  logputs (LOG_VERBOSE, !authoritative ? _(" (unauthoritative)\n") : "\n");
 }
 
 /* Retrieves a file with denoted parameters through opening an FTP
