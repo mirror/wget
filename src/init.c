724e2f84be7d2e9297c3993a9d4b5de38b544ba6
hniksic <devnull@localhost>
2005-05-21 06:36:43 -0700

    [svn] Report exact command name alongside the "display name".

--- src/init.c
+++ src/init.c
@@ -612,7 +612,7 @@ static int
 setval_internal (int comind, const char *com, const char *val)
 {
   assert (0 <= comind && comind < countof (commands));
-  DEBUGP (("Setting %s (%d) to %s\n", com, comind, val));
+  DEBUGP (("Setting %s (%s) to %s\n", com, commands[comind].name, val));
   return ((*commands[comind].action) (com, val, commands[comind].place));
 }
 
