134e59c61a1fae89522dba0c832eb5321e5fce8d
hniksic <devnull@localhost>
2005-07-04 14:41:41 -0700

    [svn] Doc fix.

--- src/wget.h
+++ src/wget.h
@@ -243,7 +243,7 @@ typedef double SUM_SIZE_INT;
 } while (0)
 
 /* Used to print pointers (usually for debugging).  Print pointers
-   using printf ("%0*lx", PTR_FORMAT (p)).  (%p is too unpredictable;
+   using printf ("0x%0*lx", PTR_FORMAT (p)).  (%p is too unpredictable;
    some implementations prepend 0x, while some don't, and most don't
    0-pad the address.)  */
 #define PTR_FORMAT(p) (int) (2 * sizeof (void *)), (unsigned long) (p)
