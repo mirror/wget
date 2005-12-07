823228830e57766ebabe529b75765816cb2507dc
mtortonesi <devnull@localhost>
2005-12-07 00:24:24 -0800

    [svn] Fixed pre-download verbose output which was broken by HTTP code refactoring.

--- src/http.c
+++ src/http.c
@@ -2157,12 +2157,30 @@ http_loop (struct url *u, char **newloc, char **local_file, const char *referer,
       if (opt.verbose)
         {
           char *hurl = url_string (u, true);
-          char tmp[256];
-          strcpy (tmp, "        ");
-          if (count > 1)
-            sprintf (tmp, _("(try:%2d)"), count);
-          logprintf (LOG_VERBOSE, "--%s--  %s\n  %s\n",
-                     tms, hurl, tmp);
+          logprintf (LOG_VERBOSE, "--%s--  %s\n",
+                     tms, hurl);
+          
+          if (count > 1) 
+            {
+              char tmp[256];
+              sprintf (tmp, _("(try:%2d)"), count);
+              logprintf (LOG_VERBOSE, "  %s", tmp);
+            }
+          else 
+            {
+              logprintf (LOG_VERBOSE, "        ");
+            }
+          
+          if (hstat.local_file) 
+            {
+              logprintf (LOG_VERBOSE, " => `%s'\n", 
+                         HYPHENP (hstat.local_file) ? "STDOUT" : hstat.local_file);
+            }
+          else
+            {
+              logprintf (LOG_VERBOSE, "\n");
+            }
+          
 #ifdef WINDOWS
           ws_changetitle (hurl);
 #endif
