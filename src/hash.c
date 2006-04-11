2a58c7d2077c878cde6513cebbef5669716952d4
hniksic <devnull@localhost>
2006-04-11 11:03:14 -0700

    [svn] Wrap macro arg in parentheses.

--- src/hash.c
+++ src/hash.c
@@ -54,7 +54,7 @@ so, delete this exception statement from your version.  */
 #  define countof(x) (sizeof (x) / sizeof ((x)[0]))
 # endif
 # include <ctype.h>
-# define TOLOWER(x) tolower ((unsigned char) x)
+# define TOLOWER(x) tolower ((unsigned char) (x))
 # if __STDC_VERSION__ >= 199901L
 #  include <stdint.h>  /* for uintptr_t */
 # else
