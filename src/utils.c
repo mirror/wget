272a9a2d9089cd66d234dfd56ea68137dd145591
hniksic <devnull@localhost>
2006-06-21 01:32:55 -0700

    [svn] Doc fix.

--- src/utils.c
+++ src/utils.c
@@ -1903,15 +1903,13 @@ base64_encode (const void *data, int length, char *dest)
 {
   /* Conversion table.  */
   static const char tbl[64] = {
-    'A','B','C','D','E','F','G','H',
-    'I','J','K','L','M','N','O','P',
-    'Q','R','S','T','U','V','W','X',
-    'Y','Z','a','b','c','d','e','f',
-    'g','h','i','j','k','l','m','n',
-    'o','p','q','r','s','t','u','v',
-    'w','x','y','z','0','1','2','3',
-    '4','5','6','7','8','9','+','/'
+    'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P',
+    'Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f',
+    'g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v',
+    'w','x','y','z','0','1','2','3','4','5','6','7','8','9','+','/'
   };
+  /* Access bytes in DATA as unsigned char, otherwise the shifts below
+     don't work for data with MSB set. */
   const unsigned char *s = data;
   /* Theoretical ANSI violation when length < 3. */
   const unsigned char *end = (const unsigned char *) data + length - 2;
