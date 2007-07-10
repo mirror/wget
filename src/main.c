0b2296c0d40b8b9c2adca7297dce781bfa397193
micah <devnull@localhost>
2007-07-10 12:30:47 -0700

    [svn] version: terminate sentence with a period, improve GPL URL, reflow a bit.

--- src/main.c
+++ src/main.c
@@ -665,7 +665,8 @@ print_version (void)
   fputs (_("\
 Copyright (C) 2007 Free Software Foundation, Inc.\n"), stdout);
   fputs (_("\
-License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>\n\
+License GPLv3+: GNU GPL version 3 or later\n\
+<http://www.gnu.org/licenses/gpl.html>.\n\
 This is free software: you are free to change and redistribute it.\n\
 There is NO WARRANTY, to the extent permitted by law.\n"), stdout);
   fputs (_("\nOriginally written by Hrvoje Niksic <hniksic@xemacs.org>.\n"),
