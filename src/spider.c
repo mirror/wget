676da61f1dff0bfa0f84e9058139cc4915a7634a
micah <devnull@localhost>
2007-08-26 16:03:58 -0700

    [svn] Added spider.c to POTFILES.in; fixed use of gettext with plural forms.

--- src/spider.c
+++ src/spider.c
@@ -140,16 +140,10 @@ print_broken_links (void)
   
   num_elems = hash_table_count (nonexisting_urls_set);
   assert (num_elems > 0);
-  
-  if (num_elems > 1) 
-    {
-      logprintf (LOG_NOTQUIET, _("Found %d broken links.\n\n"), 
-                 num_elems);
-    }
-  else
-    {
-      logprintf (LOG_NOTQUIET, _("Found 1 broken link.\n\n"));
-    }
+
+  logprintf (LOG_NOTQUIET, ngettext("Found %d broken link.\n\n",
+                                    "Found %d broken links.\n\n", num_elems),
+             num_elems);
   
   for (hash_table_iterate (nonexisting_urls_set, &iter);
        hash_table_iter_next (&iter); )
