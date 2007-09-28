ebf43dc18b09dde34711298e5c6276c04510f995
Micah Cowan <micah@cowan.name>
2007-09-28 14:56:06 -0700

    Adjust GPL invocation in msdos/config.h

--- msdos/config.h
+++ msdos/config.h
@@ -1,20 +1,20 @@
 /* Configuration header file for MS-DOS/Watt-32
+   Copyright (C) 2007 Free Software Foundation, Inc.
 
-   Copyright (C) 1995, 1996, 1997, 1998 Free Software Foundation, Inc.
+   This file is part of GNU Wget.
 
-   This program is free software; you can redistribute it and/or modify
+   GNU Wget is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2 of the License, or
+   the Free Software Foundation; either version 3 of the License, or
    (at your option) any later version.
 
-   This program is distributed in the hope that it will be useful,
+   GNU Wget is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+   along with Wget.  If not, see <http://www.gnu.org/licenses/>.
 
    In addition, as a special exception, the Free Software Foundation
    gives permission to link the code of its release of Wget with the
