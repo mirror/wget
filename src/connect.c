1153d3d05c2129f50b5e4a81d1f021986a2a19d0
Micah Cowan <micah@cowan.name>
2007-10-02 22:12:04 -0700

    Warn about host resolution failure.

--- src/connect.c
+++ src/connect.c
@@ -362,7 +362,12 @@ connect_to_host (const char *host, int port)
 
  retry:
   if (!al)
-    return E_HOST;
+    {
+      logprintf (LOG_NOTQUIET,
+                 _("%s: unable to resolve host address `%s'\n"),
+                 exec_name, host);
+      return E_HOST;
+    }
 
   address_list_get_bounds (al, &start, &end);
   for (i = start; i < end; i++)
--- src/http.c
+++ src/http.c
@@ -1632,6 +1632,9 @@ gethttp (struct url *u, struct http_stat *hs, int *dt, struct url *proxy)
       if (host_lookup_failed)
         {
           request_free (req);
+          logprintf(LOG_NOTQUIET,
+                    _("%s: unable to resolve host address `%s'\n"),
+                    exec_name, relevant->host);
           return HOSTERR;
         }
 
