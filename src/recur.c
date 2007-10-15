38b0f4ba27c6c0b2a12d01df50977af05d1a83f4
Micah Cowan <micah@cowan.name>
2007-10-14 19:00:33 -0700

    Josh's patch to remove robots.txt when appriopriate.

--- src/recur.c
+++ src/recur.c
@@ -570,6 +570,15 @@ download_child_p (const struct urlpos *upos, struct url *parent, int depth,
           if (res_retrieve_file (url, &rfile))
             {
               specs = res_parse_from_file (rfile);
+
+              /* Delete the robots.txt file if we chose to either delete the
+                 files after downloading or we're just running a spider. */
+              if (opt.delete_after || opt.spider)
+                {
+                  logprintf (LOG_VERBOSE, "Removing %s.\n", rfile);
+                  unlink (rfile);
+                }
+
               xfree (rfile);
             }
           else
