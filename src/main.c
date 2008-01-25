01dfe8a75980bb739e62e303db055c4181099a7c
Micah Cowan <micah@cowan.name>
2008-01-25 04:38:56 -0800

    Notes to translators re (C) and diacritics in names.

--- src/main.c
+++ src/main.c
@@ -672,6 +672,8 @@ static void
 print_version (void)
 {
   printf ("GNU Wget %s\n\n", version_string);
+  /* TRANSLATORS: When available, an actual copyright character
+     (cirle-c) should be used in preference to "(C)". */
   fputs (_("\
 Copyright (C) 2007 Free Software Foundation, Inc.\n"), stdout);
   fputs (_("\
@@ -679,6 +681,8 @@ License GPLv3+: GNU GPL version 3 or later\n\
 <http://www.gnu.org/licenses/gpl.html>.\n\
 This is free software: you are free to change and redistribute it.\n\
 There is NO WARRANTY, to the extent permitted by law.\n"), stdout);
+  /* TRANSLATORS: When available, please use the proper diacritics for
+     names such as this one. See en_US.po for reference. */
   fputs (_("\nOriginally written by Hrvoje Niksic <hniksic@xemacs.org>.\n"),
          stdout);
   fputs (_("Currently maintained by Micah Cowan <micah@cowan.name>.\n"),
