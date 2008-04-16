b53c77dfc06f4013a6ffea3e45bbbaace231b75f
Steven Schubiger <schubiger@gmail.com>
2008-04-16 12:16:37 +0200

    Use Gnulib's quote function in log.c.

--- src/log.c
+++ src/log.c
@@ -781,8 +781,8 @@ redirect_output (void)
   logfp = unique_create (DEFAULT_LOGFILE, false, &logfile);
   if (logfp)
     {
-      fprintf (stderr, _("\n%s received, redirecting output to `%s'.\n"),
-               redirect_request_signal_name, logfile);
+      fprintf (stderr, _("\n%s received, redirecting output to %s.\n"),
+               redirect_request_signal_name, quote (logfile));
       xfree (logfile);
       /* Dump the context output to the newly opened log.  */
       log_dump_context ();
