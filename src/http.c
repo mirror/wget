b2ad06272d08e5c9f0eb0861648d0bf2455c1a28
Micah Cowan <micah@cowan.name>
2008-04-22 23:07:19 -0700

    Make http.c's handling of --quiet --server-response consistent with ftp.c's.

--- src/http.c
+++ src/http.c
@@ -810,7 +810,7 @@ print_response_line(const char *prefix, const char *b, const char *e)
 {
   char *copy;
   BOUNDED_TO_ALLOCA(b, e, copy);
-  logprintf (LOG_VERBOSE, "%s%s\n", prefix, escnonprint(copy));
+  logprintf (LOG_ALWAYS, "%s%s\n", prefix, escnonprint(copy));
 }
 
 /* Print the server response, line by line, omitting the trailing CRLF
