1897176633a207f4e84b90d954c426eb7cfb205f
Steven Schubiger <schubiger@gmail.com>
2008-04-24 21:43:09 +0200

    Further quote*()-related integrations in connect.c.

--- src/connect.c
+++ src/connect.c
@@ -268,7 +268,7 @@ connect_to_ip (const ip_address *ip, int port, const char *print)
       const char *txt_addr = print_address (ip);
       if (print && 0 != strcmp (print, txt_addr))
         logprintf (LOG_VERBOSE, _("Connecting to %s|%s|:%d... "),
-                   escnonprint (print), txt_addr, port);
+                   escnonprint_uri (print), txt_addr, port);
       else
         logprintf (LOG_VERBOSE, _("Connecting to %s:%d... "), txt_addr, port);
     }
