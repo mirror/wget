e8200e6808978486c0d177e5559b1097a7a64b8d
Micah Cowan <micah@cowan.name>
2008-05-12 23:54:48 -0700

    Downgrade -N with -O to a warning.

--- src/main.c
+++ src/main.c
@@ -900,10 +900,9 @@ will be placed in the single file you specified.\n\n"));
         }
       if (opt.timestamping)
         {
-          fputs (_("\
-Cannot specify -N if -O is given. See the manual for details.\n\n"), stdout);
-          print_usage ();
-          exit (1);
+          logprintf (LOG_NOTQUIET, "%s", _("\
+WARNING: timestamping does nothing in combination with -O. See the manual\n\
+for details.\n\n"));
         }
     }
 
