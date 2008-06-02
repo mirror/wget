de86fc8c82b0ccaeb6acde6a27f6e6b7f54a489d
Micah Cowan <micah@cowan.name>
2008-06-01 17:28:44 -0700

    Reopen stdout in binary mode for -O -.

--- src/main.c
+++ src/main.c
@@ -991,7 +991,19 @@ for details.\n\n"));
   if (opt.output_document)
     {
       if (HYPHENP (opt.output_document))
-        output_stream = stdout;
+        {
+#ifdef WINDOWS
+          FILE *result;
+          result = freopen (NULL, "wb", stdout);
+          if (result == NULL)
+            {
+              logputs (LOG_NOTQUIET, _("\
+WARNING: Can't reopen standard output in binary mode;\n\
+         downloaded file may contain inappropriate line endings.\n"));
+            }
+#endif
+          output_stream = stdout;
+        }
       else
         {
           struct_fstat st;
