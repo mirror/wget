6db8909f87012ee8f65e496846962d98cd8bafea
Micah Cowan <micah@cowan.name>
2008-06-24 12:08:46 -0700

    stsc: escnonprint -> quotearg_style

--- src/http.c
+++ src/http.c
@@ -2704,7 +2704,7 @@ Remote file exists.\n\n"));
                       logprintf (LOG_NONVERBOSE, 
                                  _("%s URL:%s %2d %s\n"), 
                                  tms, u->url, hstat.statcode,
-                                 hstat.message ? escnonprint (hstat.message) : "");
+                                 hstat.message ? quotearg_style (escape_quoting_style, hstat.message) : "");
                       goto exit;
                     }
                 }
