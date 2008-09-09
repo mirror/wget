b58816ed33c2f167ce2e199df37fe1749d67605e
Gisle Vanem <gvanem@broadpark.no>
2008-09-09 10:26:44 -0700

    aprintf, not asprintf.

--- src/url.c
+++ src/url.c
@@ -900,9 +900,9 @@ url_error (const char *url, int error_code)
       if ((p = strchr (scheme, ':')))
         *p = '\0';
       if (!strcasecmp (scheme, "https"))
-        asprintf (&error, _("HTTPS support not compiled in"));
+        error = aprintf (_("HTTPS support not compiled in"));
       else
-        asprintf (&error, _(parse_errors[error_code]), quote (scheme));
+        error = aprintf (_(parse_errors[error_code]), quote (scheme));
       xfree (scheme);
 
       return error;
