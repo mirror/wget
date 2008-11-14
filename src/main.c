94d7b0b5701687922e498de2e1a89d0d8cd262a2
Steven Schubiger <stsc@members.fsf.org>
2008-11-14 13:48:32 +0100

    Improve printing formatted build information.

--- src/main.c
+++ src/main.c
@@ -733,13 +733,7 @@ format_and_print_line (const char *prefix, const char *line,
          token on the next line. */
       if (remaining_chars <= strlen (token)) 
         {
-          int j;
-          printf ("\n");
-          j = 0;
-          for (j = 0; j < leading_spaces; j++) 
-            {
-              printf (" ");
-            }
+          printf ("\n%*c", leading_spaces, ' ');
           remaining_chars = line_length - leading_spaces;
         }
       printf ("%s ", token);
