c2fe2f415960d9bfeef045f651d957f60bbf8f9b
Micah Cowan <micah@cowan.name>
2009-07-04 15:38:42 -0700

    Fix minor version-printer merge glitch.

--- src/main.c
+++ src/main.c
@@ -780,7 +780,6 @@ print_version (void)
   char *env_wgetrc, *user_wgetrc;
   int i;
 
-  printf (_("GNU Wget %s\n\n"), version_string);
 #ifdef __VMS
   printf ("GNU Wget %s built on VMS %s %s.\n\n",
    VERSION_STRING, vms_arch(), vms_vers());
