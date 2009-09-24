e83b33f844f7bb511cb230409e5f28a7a1b92cc7
Steven Schweda <sms@antinode.info>
2009-09-24 12:23:08 -0700

    target -> targ; use OS_TYPE for VMS in build info string.

--- src/ftp.c
+++ src/ftp.c
@@ -690,7 +690,7 @@ Error in server response, closing control connection.\n"));
           if (!opt.server_response)
             logprintf (LOG_VERBOSE, "==> CWD (%d) %s ... ", cwd_count,
                        quotearg_style (escape_quoting_style, target));
-          err = ftp_cwd (csock, target);
+          err = ftp_cwd (csock, targ);
           /* FTPRERR, WRITEFAILED, FTPNSFOD */
           switch (err)
             {
--- src/main.c
+++ src/main.c
@@ -785,12 +785,7 @@ print_version (void)
   char *env_wgetrc, *user_wgetrc;
   int i;
 
-#ifdef __VMS
-  printf (_("GNU Wget %s built on VMS %s %s.\n\n"),
-   version_string, vms_arch(), vms_vers());
-#else /* def __VMS */
   printf (_("GNU Wget %s built on %s.\n\n"), version_string, OS_TYPE);
-#endif /* def __VMS */
   /* compiled_features is a char*[]. We limit the characters per
      line to MAX_CHARS_PER_LINE and prefix each line with a constant
      number of spaces for proper alignment. */
