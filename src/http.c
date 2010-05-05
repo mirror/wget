282798f52e2cde76eaeb1384d6fcbdc624c79611
Giuseppe Scrivano <gscrivano@gnu.org>
2010-05-05 12:27:29 +0200

    Give a nicer message on a malformed status line.

--- src/http.c
+++ src/http.c
@@ -1890,8 +1890,11 @@ gethttp (struct url *u, struct http_stat *hs, int *dt, struct url *proxy,
           statcode = resp_status (resp, &message);
           if (statcode < 0)
             {
-              logprintf (LOG_NOTQUIET, _("Invalid server response.\n"));
-              CLOSE_INVALIDATE (sock);
+              char *tms = datetime_str (time (NULL));
+              logprintf (LOG_VERBOSE, "%d\n", statcode);
+              logprintf (LOG_NOTQUIET, _("%s ERROR %d: %s.\n"), tms, statcode,
+                         quotearg_style (escape_quoting_style,
+                                         _("Malformed status line")));
               xfree (head);
               return HERR;
             }
@@ -1985,7 +1988,11 @@ gethttp (struct url *u, struct http_stat *hs, int *dt, struct url *proxy,
   statcode = resp_status (resp, &message);
   if (statcode < 0)
     {
-      logprintf (LOG_NOTQUIET, _("Invalid server response.\n"));
+      char *tms = datetime_str (time (NULL));
+      logprintf (LOG_VERBOSE, "%d\n", statcode);
+      logprintf (LOG_NOTQUIET, _("%s ERROR %d: %s.\n"), tms, statcode,
+                 quotearg_style (escape_quoting_style,
+                                 _("Malformed status line")));
       CLOSE_INVALIDATE (sock);
       request_free (req);
       return HERR;
