d733dbc7031aaa417e45c38a34a9b72813176bbd
Giuseppe Scrivano <gscrivano@gnu.org>
2010-05-27 12:45:15 +0200

    Exit with failure if -k is specified and -O is not a regular file.

--- src/main.c
+++ src/main.c
@@ -1217,6 +1217,13 @@ WARNING: Can't reopen standard output in binary mode;\n\
           if (fstat (fileno (output_stream), &st) == 0 && S_ISREG (st.st_mode))
             output_stream_regular = true;
         }
+      if (!output_stream_regular && opt.convert_links)
+        {
+          printf (_("-k can be used together with -O only if outputting to \
+a regular file.\n"));
+          print_usage ();
+          exit(1);
+        }
     }
 
 #ifdef __VMS
