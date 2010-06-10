e16bd204744119e70041a1165438a8c19a86b471
Giuseppe Scrivano <gscrivano@gnu.org>
2010-06-10 10:19:27 +0200

    Drop the current maintainer line from the version display.

--- src/main.c
+++ src/main.c
@@ -853,8 +853,6 @@ There is NO WARRANTY, to the extent permitted by law.\n"), stdout);
      names such as this one. See en_US.po for reference. */
   fputs (_("\nOriginally written by Hrvoje Niksic <hniksic@xemacs.org>.\n"),
          stdout);
-  fputs (_("Currently maintained by Micah Cowan <micah@cowan.name>.\n"),
-         stdout);
   fputs (_("Please send bug reports and questions to <bug-wget@gnu.org>.\n"),
          stdout);
   exit (0);
