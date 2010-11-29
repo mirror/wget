20a64d03d8f3799f5c2b761298ee332e153ebf52
Reza Snowdon <vivi@mage.me.uk>
2010-11-29 15:24:48 +0100

    Exit immediately if the system config file cannot be parsed.

--- src/init.c
+++ src/init.c
@@ -588,6 +588,15 @@ initialize (void)
   else if (file_exists_p (SYSTEM_WGETRC))
     ok &= run_wgetrc (SYSTEM_WGETRC);
 #endif
+  /* If there are any problems parsing the system wgetrc file, tell
+     the user and exit */
+  if (! ok)
+    {
+      fprintf (stderr, _("\
+Parsing system wgetrc file failed, please check '%s'.           \
+Or specify a different file using --config\n"), SYSTEM_WGETRC);
+      exit (2);
+    }
   /* Override it with your own, if one exists.  */
   file = wgetrc_file_name ();
   if (!file)
