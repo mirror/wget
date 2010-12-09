cdfd8a8e91b65632ad21c4911c44c179ab2c9b39
Giuseppe Scrivano <gscrivano@gnu.org>
2010-12-09 13:27:17 +0100

    If -nc and -k are used at the same time, use only the latter

--- src/main.c
+++ src/main.c
@@ -1060,6 +1060,14 @@ main (int argc, char **argv)
   /* All user options have now been processed, so it's now safe to do
      interoption dependency checks. */
 
+  if (opt.noclobber && opt.convert_links)
+    {
+      fprintf (stderr,
+               _("Both --no-clobber and --convert-links were specified,"
+                 "only --convert-links will be used.\n"));
+      opt.noclobber = false;
+    }
+
   if (opt.reclevel == 0)
       opt.reclevel = INFINITE_RECURSION; /* see recur.h for commentary */
 
