09bc5b5787a763413cf3dec387fa6f3698798241
Gilles Carry <gilles.carry@st.com>
2011-02-24 09:40:31 +0100

    Use stderr instead of stdout to prompt password.

--- src/main.c
+++ src/main.c
@@ -740,9 +740,9 @@ static char *
 prompt_for_password (void)
 {
   if (opt.user)
-    printf (_("Password for user %s: "), quote (opt.user));
+    fprintf (stderr, _("Password for user %s: "), quote (opt.user));
   else
-    printf (_("Password: "));
+    fprintf (stderr, _("Password: "));
   return getpass("");
 }
 
