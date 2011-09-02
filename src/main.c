a25607f788fe8b31f954ea8174f92b77e2dd8a59
Mojca Miklavec <mojca.miklavec.lists@gmail.com>
2011-09-02 13:37:10 +0200

    Now --version works again.

--- src/main.c
+++ src/main.c
@@ -824,7 +824,7 @@ print_version (void)
       int line_length = MAX_CHARS_PER_LINE;
       while ((line_length > 0) && (compiled_features[i] != NULL))
         {
-          if (printf ("%s ", compiled_features[i]))
+          if (printf ("%s ", compiled_features[i]) < 0)
             exit (3);
           line_length -= strlen (compiled_features[i]) + 2;
           i++;
