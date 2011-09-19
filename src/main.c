91aeb7db2f7137659a76f48708f4693daad64596
Giuseppe Scrivano <gscrivano@gnu.org>
2011-09-19 17:29:33 +0200

    Fix copyright year.

--- src/main.c
+++ src/main.c
@@ -884,7 +884,7 @@ print_version (void)
   /* TRANSLATORS: When available, an actual copyright character
      (cirle-c) should be used in preference to "(C)". */
   if (fputs (_("\
-Copyright (C) 2009 Free Software Foundation, Inc.\n"), stdout) < 0)
+Copyright (C) 2011 Free Software Foundation, Inc.\n"), stdout) < 0)
     exit (3);
   if (fputs (_("\
 License GPLv3+: GNU GPL version 3 or later\n\
