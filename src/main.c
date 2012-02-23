bcc2abf116f565d333ac3bdc64c10f5a7e1990fb
Giuseppe Scrivano <gscrivano@gnu.org>
2012-02-23 11:45:05 +0100

    Handle correctly some malloc failures.

--- src/main.c
+++ src/main.c
@@ -988,15 +988,20 @@ main (int argc, char **argv)
   for (i = 1; i < argc; i++)
     argstring_length += strlen (argv[i]) + 2 + 1;
   char *p = program_argstring = malloc (argstring_length * sizeof (char));
+  if (p == NULL)
+    {
+      fprintf (stderr, _("Memory allocation problem\n"));
+      exit (2);
+    }
   for (i = 1; i < argc; i++)
-  {
-    *p++ = '"';
-    int arglen = strlen (argv[i]);
-    memcpy (p, argv[i], arglen);
-    p += arglen;
-    *p++ = '"';
-    *p++ = ' ';
-  }
+    {
+      *p++ = '"';
+      int arglen = strlen (argv[i]);
+      memcpy (p, argv[i], arglen);
+      p += arglen;
+      *p++ = '"';
+      *p++ = ' ';
+    }
   *p = '\0';
 
   /* Load the hard-coded defaults.  */
@@ -1355,6 +1360,11 @@ for details.\n\n"));
 
   /* Fill in the arguments.  */
   url = alloca_array (char *, nurl + 1);
+  if (url == NULL)
+    {
+      fprintf (stderr, _("Memory allocation problem\n"));
+      exit (2);
+    }
   for (i = 0; i < nurl; i++, optind++)
     {
       char *rewritten = rewrite_shorthand_url (argv[optind]);
