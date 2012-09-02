74f6cb88841b63ee9399e249ee993ac31a262a7f
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2012-09-02 16:33:09 +0700

    Mark more strings for translation

--- src/main.c
+++ src/main.c
@@ -1049,7 +1049,7 @@ main (int argc, char **argv)
             }
           if (!userrc_ret)
             {
-              fprintf (stderr, "Exiting due to error in %s\n", optarg);
+              fprintf (stderr, _("Exiting due to error in %s\n"), optarg);
               exit (2);
             }
           else
--- src/recur.c
+++ src/recur.c
@@ -641,7 +641,7 @@ download_child_p (const struct urlpos *upos, struct url *parent, int depth,
                  files after downloading or we're just running a spider. */
               if (opt.delete_after || opt.spider)
                 {
-                  logprintf (LOG_VERBOSE, "Removing %s.\n", rfile);
+                  logprintf (LOG_VERBOSE, _("Removing %s.\n"), rfile);
                   if (unlink (rfile))
                       logprintf (LOG_NOTQUIET, "unlink: %s\n",
                                  strerror (errno));
