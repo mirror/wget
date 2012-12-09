b56a528aea6287a9fe5b7b339a4b5ba5d0b9b470
Giuseppe Scrivano <gscrivano@gnu.org>
2012-12-09 17:04:39 +0100

    Do not honor --config only when used as first long argument

--- src/main.c
+++ src/main.c
@@ -1034,7 +1034,6 @@ main (int argc, char **argv)
                                 short_options, long_options, &longindex)) != -1)
     {
       int confval;
-      bool userrc_ret = true;
       struct cmdline_option *config_opt;
 
       /* There is no short option for "--config". */
@@ -1044,16 +1043,17 @@ main (int argc, char **argv)
           config_opt = &option_data[confval & ~BOOLEAN_NEG_MARKER];
           if (strcmp (config_opt->long_name, "config") == 0)
             {
+              bool userrc_ret = true;
               userrc_ret &= run_wgetrc (optarg);
               use_userconfig = true;
+              if (userrc_ret)
+                break;
+              else
+                {
+                  fprintf (stderr, _("Exiting due to error in %s\n"), optarg);
+                  exit (2);
+                }
             }
-          if (!userrc_ret)
-            {
-              fprintf (stderr, _("Exiting due to error in %s\n"), optarg);
-              exit (2);
-            }
-          else
-            break;
         }
     }
 
