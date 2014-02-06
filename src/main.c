d68295bdb1571589698ecd0f887f4ba6b72e4d30
Giuseppe Scrivano <gscrivan@redhat.com>
2014-02-06 18:35:05 +0100

    wget: move --version copyright year out of the localized string

--- src/main.c
+++ src/main.c
@@ -952,8 +952,8 @@ print_version (void)
 
   /* TRANSLATORS: When available, an actual copyright character
      (circle-c) should be used in preference to "(C)". */
-  if (fputs (_("\
-Copyright (C) 2011 Free Software Foundation, Inc.\n"), stdout) < 0)
+  if (printf (_("\
+Copyright (C) %s Free Software Foundation, Inc.\n"), "2014") < 0)
     exit (3);
   if (fputs (_("\
 License GPLv3+: GNU GPL version 3 or later\n\
