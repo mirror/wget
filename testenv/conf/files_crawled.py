825716399c4ce86941c5b5b123070b1c92a20c43
Darshit Shah <darnir@gmail.com>
2014-06-22 01:06:08 +0530

    Fix crash in test suite

--- testenv/conf/files_crawled.py
+++ testenv/conf/files_crawled.py
@@ -14,5 +14,5 @@ class FilesCrawled:
             diff = headers.symmetric_difference(remaining)
 
             if diff:
-                print_red(diff)
+                print_red (str(diff))
                 raise TestFailed('Not all files were crawled correctly.')
