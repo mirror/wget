2457715e7d282411aef54a3ac1982181406c511a
Tim Rühsen <tim.ruehsen@gmx.de>
2014-11-10 15:34:46 +0100

    Fix OpenSSL compile-time check for TLSv1.1 and TLSv1.2
    Reported-by: Velemas Vosak <velemas@gmail.com>

--- src/openssl.c
+++ src/openssl.c
@@ -220,13 +220,20 @@ ssl_init (void)
     case secure_protocol_tlsv1:
       meth = TLSv1_client_method ();
       break;
-#if OPENSSL_VERSION_NUMBER >= 0x01001000
+#if OPENSSL_VERSION_NUMBER >= 0x10001000
     case secure_protocol_tlsv1_1:
       meth = TLSv1_1_client_method ();
       break;
     case secure_protocol_tlsv1_2:
       meth = TLSv1_2_client_method ();
       break;
+#else
+    case secure_protocol_tlsv1_1:
+		logprintf (LOG_NOTQUIET, _("Your OpenSSL version is too old to support TLSv1.1\n"));
+		goto error;
+    case secure_protocol_tlsv1_2:
+		logprintf (LOG_NOTQUIET, _("Your OpenSSL version is too old to support TLSv1.2\n"));
+		goto error;
 #endif
     default:
       abort ();
