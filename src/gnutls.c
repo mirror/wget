f518d6cea167cac0f66d6e5fbc5cdb30096db50e
Tim Rühsen <tim.ruehsen@gmx.de>
2014-11-18 14:23:27 +0100

    Report load failure of cert files only with --debug
    Fixes #41331

--- src/gnutls.c
+++ src/gnutls.c
@@ -138,8 +138,7 @@ ssl_init (void)
               hash_table_put (inode_map, (void *)(intptr_t) st.st_ino, NULL);
               if ((rc = gnutls_certificate_set_x509_trust_file (credentials, ca_file,
                                                                 GNUTLS_X509_FMT_PEM)) <= 0)
-                logprintf (LOG_NOTQUIET, _("ERROR: Failed to open cert %s: (%d).\n"),
-                           ca_file, rc);
+                DEBUGP (("WARNING: Failed to open cert %s: (%d).\n", ca_file, rc));
               else
                 ncerts += rc;
             }
