aaefe8bc83b884e4b05274abd0ca948f9450f71f
Darshit Shah <darnir@gmail.com>
2014-12-04 18:46:44 +0530

    Remove illogical assertion in ftp.c
    A call to assert(1) will always fail and cause Wget to crash. If such a
    situation does arise, Wget should invoke abort() and provide a useful
    error message to the user prior to exiting.

--- src/ftp.c
+++ src/ftp.c
@@ -724,8 +724,10 @@ Error in server response, closing control connection.\n"));
                 break;
 
               default:
-                /* Can't happen. */
-                assert (1);
+                logprintf (LOG_ALWAYS, _("Logically impossible section reached in getftp()"));
+                logprintf (LOG_ALWAYS, _("cwd_count: %d\ncwd_start: %d\ncwd_end: %d\n"),
+                                         cwd_count, cwd_start, cwd_end);
+                abort ();
             }
 
           if (!opt.server_response)
