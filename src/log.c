mirror/wget

commit 0b5b100fc9aaa54257ffa16a083a862c031c335b

Exit immediately on a SIGPIPE error while logging.

src/log.c
@@ -499,6 +499,9 @@ logprintf (enum log_options o, const char *fmt, ...)
       va_start (args, fmt);
       done = log_vprintf_internal (&lpstate, fmt, args);
       va_end (args);
+
+      if (done && errno == EPIPE)
+        exit (1);
     }
   while (!done);
 }

commit ec5e0f421c88e8f2194e0de0a00ce82018ecfa2c

src/log.c: Fix unexpected termination in logprintf (tiny change)
When errno was set to EPIPE before call to logprintf (e.g. during close of
SSL connection that was reset by peer), it will unexpectedly terminate wget.
It should exit only when EPIPE was triggered by logging code.

Regression by 0b5b100fc9aaa54257ffa16a083a862c031c335b

src/log.c
@@ -536,6 +536,7 @@ logprintf (enum log_options o, const char *fmt, ...)
   CHECK_VERBOSE (o);
 
   xzero (lpstate);
+  errno = 0;
   do
     {
       va_start (args, fmt);