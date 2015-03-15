e04c5989ff482e1fd1e6ee9defae94c95893d5bd
Miquel Llobet <mllobet.cm@gmail.com>
2015-03-15 21:48:27 +0100

    Fixed #44516 -o- not logging to stdout
    src/log.c (log_init): check for hypen on filename, set stdout

--- src/log.c
+++ src/log.c
@@ -598,11 +598,18 @@ log_init (const char *file, bool appendp)
 {
   if (file)
     {
-      logfp = fopen (file, appendp ? "a" : "w");
-      if (!logfp)
+      if (HYPHENP (file))
         {
-          fprintf (stderr, "%s: %s: %s\n", exec_name, file, strerror (errno));
-          exit (WGET_EXIT_GENERIC_ERROR);
+          logfp = stdout;
+        }
+      else
+        {
+          logfp = fopen (file, appendp ? "a" : "w");
+          if (!logfp)
+            {
+              fprintf (stderr, "%s: %s: %s\n", exec_name, file, strerror (errno));
+              exit (WGET_EXIT_GENERIC_ERROR);
+            }
         }
     }
   else
