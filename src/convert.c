030c3379d198fca9a3138554e6fdd3a3be8482e9
Jookia <166291@gmail.com>
2015-08-22 02:29:05 +1000

    Clarify that links are being converted.
    * src/convert.c: Add 'links in' after 'Converted %d' and 'Converting %s'.

--- src/convert.c
+++ src/convert.c
@@ -193,7 +193,7 @@ convert_all_links (void)
   convert_links_in_hashtable (downloaded_css_set, 1, &file_count);
 
   secs = ptimer_measure (timer);
-  logprintf (LOG_VERBOSE, _("Converted %d files in %s seconds.\n"),
+  logprintf (LOG_VERBOSE, _("Converted links in %d files in %s seconds.\n"),
              file_count, print_decimal (secs));
 
   ptimer_destroy (timer);
@@ -221,7 +221,7 @@ convert_links (const char *file, struct urlpos *links)
   struct urlpos *link;
   int to_url_count = 0, to_file_count = 0;
 
-  logprintf (LOG_VERBOSE, _("Converting %s... "), file);
+  logprintf (LOG_VERBOSE, _("Converting links in %s... "), file);
 
   {
     /* First we do a "dry run": go through the list L and see whether
