7bed9a6f8f106d88737284850941f9aa6d21a13b
Tim Rühsen <tim.ruehsen@gmx.de>
2015-08-26 10:35:57 +0200

    Suppress debug output when strings may contain password
    * iri.c (do_conversion): Do not print out converted strings if they
      contain an '@'. That could be an URL with embedded password.
    
    Fixes #45825

--- src/iri.c
+++ src/iri.c
@@ -152,7 +152,14 @@ do_conversion (const char *tocode, const char *fromcode, char const *in_org, siz
           *(s + len - outlen - done) = '\0';
           xfree(in_save);
           iconv_close(cd);
-          DEBUGP (("converted '%s' (%s) -> '%s' (%s)\n", in_org, fromcode, *out, tocode));
+          IF_DEBUG
+          {
+            /* not not print out embedded passwords, in_org might be an URL */
+            if (!strchr(in_org, '@') && !strchr(*out, '@'))
+              debug_logprintf ("converted '%s' (%s) -> '%s' (%s)\n", in_org, fromcode, *out, tocode);
+            else
+              debug_logprintf ("%s: logging suppressed, strings may contain password\n", __func__);
+          }
           return true;
         }
 
@@ -193,7 +200,14 @@ do_conversion (const char *tocode, const char *fromcode, char const *in_org, siz
 
     xfree(in_save);
     iconv_close(cd);
-    DEBUGP (("converted '%s' (%s) -> '%s' (%s)\n", in_org, fromcode, *out, tocode));
+    IF_DEBUG
+    {
+      /* not not print out embedded passwords, in_org might be an URL */
+      if (!strchr(in_org, '@') && !strchr(*out, '@'))
+        debug_logprintf ("converted '%s' (%s) -> '%s' (%s)\n", in_org, fromcode, *out, tocode);
+      else
+        debug_logprintf ("%s: logging suppressed, strings may contain password\n", __func__);
+    }
     return false;
 }
 
