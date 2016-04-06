33d860e1ef9e35708e3834c6e8d1f70a74c7892c
Ander Juaristi <ajuaristi@gmx.es>
2016-04-06 13:13:52 +0200

    Correct HSTS database file description
     * src/hsts.c (hsts_store_dump): s/[:port]/<port>/

--- src/hsts.c
+++ src/hsts.c
@@ -316,7 +316,7 @@ hsts_store_dump (hsts_store_t store, FILE *fp)
   /* Print preliminary comments. We don't care if any of these fail. */
   fputs ("# HSTS 1.0 Known Hosts database for GNU Wget.\n", fp);
   fputs ("# Edit at your own risk.\n", fp);
-  fputs ("# <hostname>[:<port>]\t<incl. subdomains>\t<created>\t<max-age>\n", fp);
+  fputs ("# <hostname>\t<port>\t<incl. subdomains>\t<created>\t<max-age>\n", fp);
 
   /* Now cycle through the HSTS store in memory and dump the entries */
   for (hash_table_iterate (store->table, &it); hash_table_iter_next (&it);)
