96ab9cad882f34c77fca0ba8d6ea0daf1803fb94
Sergio Gelato <sergio.gelato@astro.su.se>
2016-04-17 15:28:48 +0200

    More accurate log message from do_conversion()
    * src/iri.c (do_conversion): More accurate log message

--- src/iri.c
+++ src/iri.c
@@ -129,8 +129,8 @@ do_conversion (const char *tocode, const char *fromcode, char const *in_org, siz
   cd = iconv_open (tocode, fromcode);
   if (cd == (iconv_t)(-1))
     {
-      logprintf (LOG_VERBOSE, _("Conversion from %s to UTF-8 isn't supported\n"),
-                 quote (opt.locale));
+      logprintf (LOG_VERBOSE, _("Conversion from %s to %s isn't supported\n"),
+                 quote (fromcode), quote (tocode));
       *out = NULL;
       return false;
     }
