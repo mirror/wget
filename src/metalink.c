e3fb4c3859d2705fb2de80801b0bba2b64bea1a1
Tim Rühsen <tim.ruehsen@gmx.de>
2016-08-04 13:09:28 +0200

    * src/metalink.c (badhash_suffix): Fix quoting

--- src/metalink.c
+++ src/metalink.c
@@ -566,7 +566,8 @@ badhash_suffix (char *name)
   bhash = concat_strings (name, ".badhash", (char *)0);
   uname = unique_name (bhash, false);
 
-  logprintf (LOG_VERBOSE, _("Renaming ‘%s’ to ‘%s’.\n"), name, uname);
+  logprintf (LOG_VERBOSE, _("Renaming %s to %s.\n"),
+             quote_n (0, name), quote_n (1, uname));
 
   if (link (name, uname))
     logprintf (LOG_NOTQUIET, "link: %s\n", strerror (errno));
