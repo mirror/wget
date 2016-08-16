f3f349a0cf0598fdf3a40d909075974c7bc992bc
Matthew White <mehw.is.me@inventati.org>
2016-08-16 23:12:56 +0200

    Add file size computation in Metalink module
    * NEWS: Mention Metalink's file size verification
    * src/metalink.c (retrieve_from_metalink): Add file size computation
    * doc/metalink.txt: Update document. Remove resolved bugs
    
    Reject downloaded files when they do not agree with their Metalink/XML
    metalink:size: https://tools.ietf.org/html/rfc5854#section-4.2.14
    
    At the moment of writing, Metalink/HTTP headers do not provide a file
    size field. This information could be obtained from the Content-Length
    header field: https://tools.ietf.org/html/rfc6249#section-7

--- src/metalink.c
+++ src/metalink.c
@@ -240,6 +240,41 @@ retrieve_from_metalink (const metalink_t* metalink)
                   continue;
                 }
 
+              logprintf (LOG_VERBOSE, _("Computing size for %s\n"), quote (filename));
+
+              if (!mfile->size)
+                {
+                  logprintf (LOG_VERBOSE, _("File size not declared. Skipping check.\n"));
+                }
+              else
+                {
+                  wgint local_file_size = file_size (filename);
+
+                  if (local_file_size == -1)
+                    {
+                      logprintf (LOG_NOTQUIET, _("Could not get downloaded file's size.\n"));
+                      fclose (local_file);
+                      local_file = NULL;
+                      continue;
+                    }
+
+                  /* FIXME: what about int64?  */
+                  DEBUGP (("Declared size: %lld\n", mfile->size));
+                  DEBUGP (("Computed size: %lld\n", (long long) local_file_size));
+
+                  if (local_file_size != (wgint) mfile->size)
+                    {
+                      logprintf (LOG_NOTQUIET, _("Size mismatch for file %s.\n"), quote (filename));
+                      fclose (local_file);
+                      local_file = NULL;
+                      continue;
+                    }
+                  else
+                    {
+                      logputs (LOG_VERBOSE, _("Size matches.\n"));
+                    }
+                }
+
               for (mchksum_ptr = mfile->checksums; *mchksum_ptr; mchksum_ptr++)
                 {
                   char md2[MD2_DIGEST_SIZE];
