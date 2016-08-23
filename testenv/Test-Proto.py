ca1ee7d32f92d9ee4e0c0d27fb923ae8e56c2466
Dale R. Worley <worley@ariadne.com>
2016-08-23 17:48:05 -0400

    Corrections and amplifications to test documentation
        * testenv/README: Update documentation to meet current project
        status
        * testenv/Test-Proto.py: Same

--- testenv/Test-Proto.py
+++ testenv/Test-Proto.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from sys import exit
 from test.http_test import HTTPTest
-from misc.constants import HTTP, HTTPS
+from test.base_test import HTTP, HTTPS
 from misc.wget_file import WgetFile
 
 """
