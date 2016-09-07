49af22ca94570da3fa43c98e92ec0830f786db0d
Tim Rühsen <tim.ruehsen@gmx.de>
2016-09-07 09:31:43 +0200

    * src/http.c (check_file_output): Replace asprintf by aprint

--- src/http.c
+++ src/http.c
@@ -2263,8 +2263,7 @@ check_file_output (struct url *u, struct http_stat *hs,
   hs->temporary = opt.delete_after || opt.spider || !acceptable (hs->local_file);
   if (hs->temporary)
     {
-      char *tmp = NULL;
-      asprintf (&tmp, "%s.tmp", hs->local_file);
+      char *tmp = aprintf ("%s.tmp", hs->local_file);
       xfree (hs->local_file);
       hs->local_file = tmp;
     }
