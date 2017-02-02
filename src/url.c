d0e02a54aedf077b27e930bec4d5abc3fb565a3c
Tim Rühsen <tim.ruehsen@gmx.de>
2017-02-02 11:11:50 +0100

    * src/url.c (mkalldirs): Add newline to log message

--- src/url.c
+++ src/url.c
@@ -1284,7 +1284,7 @@ mkalldirs (const char *path)
     }
   res = make_directory (t);
   if (res != 0)
-    logprintf (LOG_NOTQUIET, "%s: %s", t, strerror (errno));
+    logprintf (LOG_NOTQUIET, "%s: %s\n", t, strerror (errno));
   xfree (t);
   return res;
 }
