e69808256bf958dc6c8e2637ca1ae7623421023f
Tim Rühsen <tim.ruehsen@gmx.de>
2017-02-03 11:53:28 +0100

    * src/css-url.h: Remove redundant declaration

--- src/css-url.h
+++ src/css-url.h
@@ -32,7 +32,6 @@ as that of the covered work.  */
 #define CSS_URL_H
 
 void get_urls_css (struct map_context *, int, int);
-void get_urls_css (struct map_context *, int, int);
 struct urlpos *get_urls_css_file (const char *, const char *);
 
 #endif /* CSS_URL_H */
