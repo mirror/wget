5d4ada1b7b0b79f8053f3d6ffddda2e2c66d9dce
Tim Rühsen <tim.ruehsen@gmx.de>
2017-05-16 10:24:52 +0200

    Fix two Metalink tests if $HOME is changed
    * conf/expected_files.py (gen_local_fs_snapshot): Skip processing
      of 'pubring.kbx'

--- testenv/conf/expected_files.py
+++ testenv/conf/expected_files.py
@@ -24,6 +24,11 @@ class ExpectedFiles:
         snapshot = {}
         for parent, dirs, files in os.walk('.'):
             for name in files:
+                # pubring.kbx will be created by libgpgme if $HOME doesn't contain the .gnupg directory.
+					 # setting $HOME to CWD (in base_test.py) breaks two Metalink tests, so we skip this file here.
+                if name == 'pubring.kbx':
+                    continue
+
                 f = {'content': ''}
                 file_path = os.path.join(parent, name)
                 with open(file_path) as fp:
