3ad3b3e36c89ae86cffba4e214fb9d7f33f4fc96
Tim Rühsen <tim.ruehsen@gmx.de>
2017-07-28 17:11:26 +0200

    * src/url.c (url_scheme): Use ASCII version of strncasecmp

--- src/url.c
+++ src/url.c
@@ -459,7 +459,7 @@ url_scheme (const char *url)
   int i;
 
   for (i = 0; supported_schemes[i].leading_string; i++)
-    if (0 == strncasecmp (url, supported_schemes[i].leading_string,
+    if (0 == c_strncasecmp (url, supported_schemes[i].leading_string,
                           strlen (supported_schemes[i].leading_string)))
       {
         if (!(supported_schemes[i].flags & scm_disabled))
