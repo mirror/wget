e0860dd1ff306abacb3780ddc6cf169cb588f328
Tim Rühsen <tim.ruehsen@gmx.de>
2018-04-15 22:04:16 +0200

    * src/init.c (cmd_bytes_sum): Fix integer over- and underflow

--- src/init.c
+++ src/init.c
@@ -1342,7 +1342,9 @@ static bool
 cmd_bytes_sum (const char *com, const char *val, void *place)
 {
   double byte_value;
-  if (!parse_bytes_helper (val, &byte_value))
+
+  if (!parse_bytes_helper (val, &byte_value)
+      || byte_value < LONG_MIN || byte_value > LONG_MAX)
     {
       fprintf (stderr, _("%s: %s: Invalid byte value %s\n"),
                exec_name, com, quote (val));
