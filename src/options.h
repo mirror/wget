221f1ba3efb9ea7cdccdd35bd29fd659ddb8a182
Darshit Shah <darnir@gnu.org>
2020-01-10 12:56:08 +0100

    Update copyright year to 2020

--- src/options.h
+++ src/options.h
@@ -1,5 +1,5 @@
 /* struct options.
-   Copyright (C) 1996-2011, 2015, 2018-2019 Free Software Foundation,
+   Copyright (C) 1996-2011, 2015, 2018-2020 Free Software Foundation,
    Inc.
 
 This file is part of GNU Wget.
