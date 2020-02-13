f460e1d04963dce88b9711002c655497a8a22390
Tim Rühsen <tim.ruehsen@gmx.de>
2020-02-13 15:17:27 +0100

    * src/main.c (main): Remove use of alloca

--- src/main.c
+++ src/main.c
@@ -1934,7 +1934,7 @@ for details.\n\n"));
     set_progress_implementation (opt.progress_type);
 
   /* Fill in the arguments.  */
-  url = alloca_array (char *, nurl + 1);
+  url = xmalloc (sizeof (char *) * (nurl + 1));
   if (url == NULL)
     {
       fprintf (stderr, _("Memory allocation problem\n"));
@@ -1946,7 +1946,7 @@ for details.\n\n"));
       if (rewritten)
         url[i] = rewritten;
       else
-        url[i] = xstrdup (argv[optind]);
+        url[i] = argv[optind];
     }
   url[i] = NULL;
 
@@ -2179,6 +2179,8 @@ only if outputting to a regular file.\n"));
         }
       iri_free (iri);
     }
+
+  xfree(url);
 
   /* And then from the input file, if any.  */
   if (opt.input_filename)
