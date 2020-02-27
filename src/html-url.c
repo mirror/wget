a3d3322a481e9f6ff40877504a273dea07a2d6dd
Tim Rühsen <tim.ruehsen@gmx.de>
2020-02-27 19:51:55 +0100

    * src/html-url.c (cleanup_html_url): Only compile for DEBUG_MALLOC or TESTING

--- src/html-url.c
+++ src/html-url.c
@@ -963,6 +963,7 @@ get_urls_file (const char *file)
   return head;
 }
 
+#if defined DEBUG_MALLOC || defined TESTING
 void
 cleanup_html_url (void)
 {
@@ -973,3 +974,4 @@ cleanup_html_url (void)
   if (interesting_attributes)
     hash_table_destroy (interesting_attributes);
 }
+#endif
