6c84dc38bc6909330feeac8365c8678383e2cca9
Darshit Shah <darnir@gnu.org>
2021-01-09 11:01:24 +0100

    maint: post-release administrivia
    * NEWS: Add header line for next release.
    * .prev-version: Record previous version.
    * cfg.mk (old_NEWS_hash): Auto-update.

--- cfg.mk
+++ cfg.mk
@@ -41,5 +41,5 @@ VC_LIST_ALWAYS_EXCLUDE_REGEX = ^ABOUT-NLS|((msdos|vms)/.*)|ChangeLog-2014-12-10|
 update-copyright-env = UPDATE_COPYRIGHT_USE_INTERVALS=1
 generated_files =
 
-old_NEWS_hash = d41d8cd98f00b204e9800998ecf8427e
+old_NEWS_hash = 0813bf0ca67740840d6385f115107fac
 manual_title = $(VERSION)
