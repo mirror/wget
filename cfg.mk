ccc7866fead1f71db48189e9004867e29aadb14e
Darshit Shah <darnir@gnu.org>
2022-02-26 16:24:57 +0100

    * cfg.mk: Remove passing syntax-checks from skip list

--- cfg.mk
+++ cfg.mk
@@ -1,35 +1,26 @@
 # Explicit syntax-check exceptions.
 exclude_file_name_regexp--sc_trailing_blank = ^doc/annou.*$$
 exclude_file_name_regexp--sc_bindtextdomain = ^fuzz/.*.c$$
-exclude_file_name_regexp--sc_require_config_h = fuzz/main.c$$
-exclude_file_name_regexp--sc_require_config_h_first = fuzz/main.c$$
+exclude_file_name_regexp--sc_require_config_h = (fuzz/.*.c|src/decc_ver.c|util/trunc.c)$$
+exclude_file_name_regexp--sc_require_config_h_first = (fuzz/.*.c|src/decc_ver.c|src/vms.c|util/trunc.c)$$
 # exclude_file_name_regexp--sc_po_
 
 
 export VC_LIST_EXCEPT_DEFAULT=^(lib/.*|m4/.*|md5/.*|build-aux/.*|src/gettext\.h|.*ChangeLog|tests/certs/.*)$$
+config_h_header = "wget\.h"
 
 local-checks-to-skip = \
     sc_const_long_option \
     sc_error_message_uppercase \
     sc_file_system \
-    sc_immutable_NEWS \
-    sc_copyright_check \
-    sc_makefile_path_separator_check \
-    sc_require_config_h \
-    sc_require_config_h_first \
     sc_two_space_separator_in_usage \
     sc_useless_cpp_parens \
     \
     sc_prohibit_atoi_atof \
     sc_prohibit_strcmp \
-    sc_cast_of_alloca_return_value \
-    sc_cast_of_x_alloc_return_value \
-    sc_prohibit_S_IS_definition \
     \
-    sc_program_name \
     sc_makefile_at_at_check \
     sc_cast_of_argument_to_free \
-    sc_prohibit_undesirable_word_seq \
     sc_prohibit_gnu_make_extensions \
 	\
 	sc_indent
