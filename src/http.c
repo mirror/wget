1cda2bb5d591f564db2f5bbfcb60ba31b6351ca0
Tim Rühsen <tim.ruehsen@gmx.de>
2022-03-20 20:53:10 +0100

    src/http.c (time_to_rfc1123): Fix -Wformat-nonliteral

--- src/http.c
+++ src/http.c
@@ -1826,8 +1826,6 @@ time_to_rfc1123 (time_t time, char *buf, size_t bufsize)
   static const char *month[] = { "Jan", "Feb", "Mar", "Apr",
                                  "May", "Jun", "Jul", "Aug",
                                  "Sep", "Oct", "Nov", "Dec" };
-  /* rfc1123 example: Thu, 01 Jan 1998 22:12:57 GMT  */
-  static const char *time_format = "%s, %02d %s %04d %02d:%02d:%02d GMT";
 
   struct tm *gtm = gmtime (&time);
   if (!gtm)
@@ -1837,7 +1835,9 @@ time_to_rfc1123 (time_t time, char *buf, size_t bufsize)
       return TIMECONV_ERR;
     }
 
-  snprintf (buf, bufsize, time_format, wkday[gtm->tm_wday],
+  /* rfc1123 example: Thu, 01 Jan 1998 22:12:57 GMT  */
+  snprintf (buf, bufsize, "%s, %02d %s %04d %02d:%02d:%02d GMT",
+            wkday[gtm->tm_wday],
             gtm->tm_mday, month[gtm->tm_mon],
             gtm->tm_year + 1900, gtm->tm_hour,
             gtm->tm_min, gtm->tm_sec);
